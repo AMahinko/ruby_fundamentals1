d8cf1eec3b08df9038b398a8191195cd935252a7
Alex <alex.mahinko@gmail.com>
2017-03-27 16:58:26 -0400

    Fixed. Exersize 4 complete.

--- exercise4.rb
+++ exercise4.rb
@@ -1,13 +1,13 @@
 number = 1
 
-while number < 100 do
-  if number % 3 && number % 5 == 0
+while number < 101 do
+  if (number % 3) == 0 && (number % 5) == 0
     puts "Bitmaker"
     number += 1
-  elsif number % 3 == 0
+  elsif (number % 3 == 0) && (number % 5 != 0)
     puts "Bit"
     number += 1
-  elsif number % 5 == 0
+  elsif (number % 5 == 0) && (number % 3 != 0)
     puts "Maker"
     number += 1
   else
