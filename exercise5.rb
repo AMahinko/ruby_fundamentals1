2e4304a566893bd8dfce2c9479646fa6273a5a02
Alex <alex.mahinko@gmail.com>
2017-03-27 17:17:37 -0400

    Exercise 5 complete. Efforts to learn how to spell exercise ongoing.

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+puts "How many pizzas?"
+pizzas=gets.to_i
+toppings_total = 0
+
+pizzas.times do
+  puts "how many toppings?"
+  toppings = gets.to_i
+  toppings_total += toppings
+  puts "You've ordered a pizza with #{toppings} toppings."
+end
+
+puts "You ordered a total of #{pizzas} with a grand total of #{toppings_total} toppings"
