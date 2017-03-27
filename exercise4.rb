39f5ea8b46a85098da7a17e1db6fd80a7ad52a01
Alex <alex.mahinko@gmail.com>
2017-03-27 16:40:03 -0400

    Program written. Trying to kill infinite loop bug.

---
+++ exercise4.rb
@@ -0,0 +1,18 @@
+number = 1
+
+while number < 100 do
+  if number % 3 && number % 5 == 0
+    puts "Bitmaker"
+    number += 1
+  elsif number % 3 == 0
+    puts "Bit"
+    number += 1
+  elsif number % 5 == 0
+    puts "Maker"
+    number += 1
+  else
+    puts number
+    number != 1
+  end
+
+end
